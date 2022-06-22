5d0021049bb5c9a4b5d369f75a7a2ae14737652b
Goldn7799 <syeifsultan@gmail.com>
2022-06-22 16:18:00 +0700

    fixbug

--- global/script/main.js
+++ global/script/main.js
@@ -70,9 +70,6 @@ const NavBar = {
         root += `<nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Riverz</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
     </div>
                 </nav>`
     },
